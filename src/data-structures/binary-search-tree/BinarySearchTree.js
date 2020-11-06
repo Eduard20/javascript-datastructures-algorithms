786a0f1ba82514606ae4c11ec20bd8f25c49517a
Eduard20 <esimonyan2014+github@gmail.com>
2020-11-07 00:28:55 +0400

    Create BinarySearchTree.js

---
+++ src/data-structures/binary-search-tree/BinarySearchTree.js
@@ -0,0 +1,34 @@
+const TreeNode = require('./TreeNode');
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  addNode(data) {
+    const newNode = new TreeNode(data);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (!node.left) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (!node.right) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+}
+
+module.exports = BinarySearchTree;
